c75431732ea89397471f8694112f64b495594a0b
ponyFan <1156447105@qq.com>
2020-07-30 11:33:34 +0800

    增加注释

--- src/main/java/com/base/Application.java
+++ src/main/java/com/base/Application.java
@@ -2,11 +2,13 @@ package com.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@EnableScheduling
+@EnableScheduling/*启用定时任务*/
+@EnableRetry/*rest重试机制*/
 @SpringBootApplication
 public class Application {
 
