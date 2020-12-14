5a2f84b009cc9b4128c063a13a6aa5a59a6f8490
fanzl <1156447105@qq.com>
2020-12-14 11:24:33 +0800

    静态文件访问

--- src/main/java/com/base/common/config/WebMvcConfig.java
+++ src/main/java/com/base/common/config/WebMvcConfig.java
@@ -29,7 +29,12 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         if (!path.endsWith("/")){
             path = path + "/";
         }
-        /*设置本地磁盘路径，如path = /root/tmp */
+        /*
+        对静态文件做一个虚拟映射，resourcehandler中参数/path/**是虚拟路径，即通过 http://localhost:8102/path/589c2c8d86cff.jpg
+        即可访问实际物理地址下的文件
+         loacation中参数则是需要指定的静态文件路径，如：file:/C:/Users/11564/Pictures/Camera Roll/  或者  linux：file:/root/fzl
+         页面只需<img src="/Path/picName.jpg" />以path开头后面文件名即具体目录下的文件名
+         */
         registry.addResourceHandler(path + "**").addResourceLocations("file:" + path);
         /*设置静态文件如html、js、css、jpg等*/
         registry.addResourceHandler("/**").addResourceLocations("/");
